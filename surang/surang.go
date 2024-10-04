8371af56dad424c1877a03bc4bd463e670c70d82
Donnie <git@donnie.in>
2024-10-04 21:44:28 +0200

    fix command context

--- surang/surang.go
+++ surang/surang.go
@@ -19,14 +19,14 @@ type Surang struct {
 // IsRunning checks if the surang is currently running.
 func (s *Surang) IsRunning(ctx context.Context) bool {
 	cmdPattern := fmt.Sprintf("ssh -f -N -D %d %s", s.Port, s.Command)
-	cmd := exec.CommandContext(ctx, "pgrep", "-f", cmdPattern)
+	cmd := exec.Command("pgrep", "-f", cmdPattern)
 	output, err := cmd.Output()
 	return err == nil && len(output) > 0
 }
 
 // Start launches the surang.
 func (s *Surang) Start(ctx context.Context) error {
-	cmd := exec.CommandContext(ctx, "ssh", "-f", "-N", "-D", strconv.Itoa(s.Port), s.Command)
+	cmd := exec.Command("ssh", "-f", "-N", "-D", strconv.Itoa(s.Port), s.Command)
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("error starting surang %s: %w", s.Name, err)
 	}
@@ -44,7 +44,7 @@ func (s *Surang) Restart(ctx context.Context) error {
 // Stop terminates the surang.
 func (s *Surang) Stop(ctx context.Context) error {
 	cmdPattern := fmt.Sprintf("ssh -f -N -D %d %s", s.Port, s.Command)
-	cmd := exec.CommandContext(ctx, "pkill", "-f", cmdPattern)
+	cmd := exec.Command("pkill", "-f", cmdPattern)
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("error stopping surang %s: %w", s.Name, err)
 	}
@@ -53,7 +53,7 @@ func (s *Surang) Stop(ctx context.Context) error {
 
 // Check verifies if the surang is correctly forwarding traffic.
 func (s *Surang) Check(ctx context.Context) (bool, error) {
-	cmd := exec.CommandContext(ctx, "curl", "-4", "-s", "--socks5", fmt.Sprintf("localhost:%d", s.Port), "icanhazip.com")
+	cmd := exec.Command("curl", "-4", "-s", "--socks5", fmt.Sprintf("localhost:%d", s.Port), "icanhazip.com")
 	output, err := cmd.Output()
 	if err != nil {
 		return false, fmt.Errorf("error checking IP for surang %s: %w", s.Name, err)
